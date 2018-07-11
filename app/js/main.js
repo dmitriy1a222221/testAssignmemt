1fe051bb15889009074f3b877870af8c415b2a45
dmitriy <dmitriy1a22222@gmail.com>
2018-07-11 22:18:47 +0300

    six commit

--- app/js/main.js
+++ app/js/main.js
@@ -5,12 +5,43 @@ document.addEventListener('DOMContentLoaded', () => {
         let counterBlock = document.querySelector('.counter');
         let elments = document.querySelectorAll('.wrapp-block li');
         let count = 0;
-        let currentEl = 1;
+        let currElement = [1];
         let arrSrollHeight = [0, 100, 200, 300, 400];
-        console.log(count);
 
+        counterBlock.innerHTML = `${1}/${elments.length}`;
 
+        document.addEventListener('keyup', (event) => {
+            const keyName = event.key;
+            if(keyName === 'ArrowUp'){
+                (function keyUp() {
+                    count--;
+                    if(count < 0) count = arrSrollHeight.length-1;
+                    currElement.splice(0, 1, currentElement());
+                    counterBlock.innerHTML = `${currElement}/${elments.length}`;
+                    console.log(currElement);
+                    wrappBlock.style.cssText = `transform: translateY(-${arrSrollHeight[count]}vh)`
+                })()
+            }
+
+        });
+        document.addEventListener('keydown', (event) => {
+            const keyName = event.key;
+            if(keyName === 'ArrowDown'){
+                (function keyDown() {
+                    count++;
+                    currElement.splice(0, 1, currentElement());
+
+                    console.log(currElement);
+                    if(count === arrSrollHeight.length) count = 0;
+                    counterBlock.innerHTML = `${currElement}/${elments.length}`;
+                    wrappBlock.style.cssText = `transform: translateY(-${arrSrollHeight[count]}vh)`
+                })()
+            }
+
+        });
         function currentElement () {
+            console.log(count);
+            var currentEl;
             switch (count) {
                 case 0:
                     currentEl = 1;
@@ -26,37 +57,11 @@ document.addEventListener('DOMContentLoaded', () => {
                     break;
                 case 4:
                     currentEl = 5;
-                break;
-
+                    break;
             }
-
+            return currentEl
         }
 
 
-        document.addEventListener('keyup', (event) => {
-            const keyName = event.key;
-            if(keyName === 'ArrowUp'){
-                (function keyUp() {
-                    count--;
-                    if(count < 0) count = arrSrollHeight.length-1;
-                    currentElement();
-                    wrappBlock.style.cssText = `transform: translateY(-${arrSrollHeight[count]}vh)`
-                })()
-            }
-
-        });
-        document.addEventListener('keydown', (event) => {
-            const keyName = event.key;
-            if(keyName === 'ArrowDown'){
-                (function keyDown() {
-                    count++;
-                    currentElement();
-                    if(count === arrSrollHeight.length) count = 0;
-                    wrappBlock.style.cssText = `transform: translateY(-${arrSrollHeight[count]}vh)`
-                })()
-            }
-
-        });
-        counterBlock.innerHTML = `${currentEl}/${elments.length}`;
     })()
 });
