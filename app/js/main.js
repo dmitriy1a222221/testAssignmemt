a6c0f08818f380cef2aa30cb1011e46a8bc265cd
dmitriy <dmitriy1a22222@gmail.com>
2018-07-11 21:19:07 +0300

    four

--- app/js/main.js
+++ app/js/main.js
@@ -3,18 +3,44 @@ document.addEventListener('DOMContentLoaded', () => {
     (function sroll() {
         let wrappBlock = document.querySelector('.wrapp-block');
         let counterBlock = document.querySelector('.counter');
-        let count = 0;
+        let elments = document.querySelectorAll('.wrapp-block li');
+        let count = {countNum: 0};
+        let currentEl = [];
         let arrSrollHeight = [0, 100, 200, 300, 400];
 
-        counterBlock.innerHTML = `${document.querySelectorAll('.wrapp-block li').length}`;
+
+        function currentElement () {
+            switch (count[0]) {
+                case 0:
+                    currentEl[0] = 1;
+                    break;
+                case 1:
+                    currentEl[0] = 2;
+                    break;
+                case 2:
+                    currentEl[0] = 3;
+                    break;
+                case 3:
+                    currentEl[0] = 4;
+                    break;
+                case 4:
+                    currentEl[0] = 5;
+                break;
+
+            }
+
+        }
+
 
         document.addEventListener('keyup', (event) => {
+            currentElement();
             const keyName = event.key;
             if(keyName === 'ArrowUp'){
                 (function keyUp() {
-                    count--;
-                    if(count <= 0) count = 0;
-                    wrappBlock.style.cssText = `transform: translateY(-${arrSrollHeight[count]}vh)`
+                    count[0]--;
+                    console.log(count[0]);
+                    if(count[0] < 0) count[0] = arrSrollHeight.length;
+                    wrappBlock.style.cssText = `transform: translateY(-${arrSrollHeight[count[0]]}vh)`
                 })()
             }
 
@@ -23,12 +49,13 @@ document.addEventListener('DOMContentLoaded', () => {
             const keyName = event.key;
             if(keyName === 'ArrowDown'){
                 (function keyDown() {
-                    count++;
-                    if(count = arrSrollHeight.length) count = arrSrollHeight.length;
-                    wrappBlock.style.cssText = `transform: translateY(-${arrSrollHeight[count]}vh)`
+                    count[0]++;
+                    if(count[0] = arrSrollHeight.length) count[0] = 0;
+                    wrappBlock.style.cssText = `transform: translateY(-${arrSrollHeight[count[0]]}vh)`
                 })()
             }
 
         });
+        counterBlock.innerHTML = `${currentEl[0]}/${elments.length}`;
     })()
 });
